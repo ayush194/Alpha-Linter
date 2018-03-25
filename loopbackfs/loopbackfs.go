5b26fb9057973b65d13bcc1b58bf14ee8d4eb038
Ayush Kumar <ayushk@Ayushs-MacBook-Air.local>
2018-03-26 00:54:36 +0530

    Rectified the buggy File Writer.

--- loopbackfs/loopbackfs.go
+++ loopbackfs/loopbackfs.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"syscall"
+	"time"
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
@@ -70,14 +71,30 @@ func (f *filesys) Root() (fs.Node, error) {
 	return &dir{f.path}, nil
 }
 
+func GetAttr(path string) (fuse.Attr, error) {
+	info, err := os.Lstat(path)
+	attr := syscall.Stat_t{}
+	err = syscall.Lstat(path, &attr)
+	return fuse.Attr{
+		//Cache time 1m0s
+		Valid:     time.Duration(60000000000),
+		Inode:     uint64(attr.Ino),
+		Mode:      os.FileMode(info.Mode()),
+		Size:      uint64(attr.Size),
+		Mtime:     time.Time(info.ModTime()),
+		Blocks:    uint64(attr.Blocks),
+		BlockSize: uint32(attr.Blksize),
+		Nlink:     uint32(attr.Nlink),
+		Uid:       uint32(attr.Uid),
+		Gid:       uint32(attr.Gid),
+		Rdev:      uint32(attr.Rdev),
+	}, err
+}
+
 func (d *dir) Attr(ctx context.Context, attr *fuse.Attr) error {
-	dir_info, _ := os.Lstat(d.path)
-	dir_attr := syscall.Stat_t{}
-	syscall.Lstat(d.path, &dir_attr)
-	attr.Inode = uint64(dir_attr.Ino)
-	attr.Mode = os.FileMode(dir_info.Mode())
-	attr.Size = uint64(dir_attr.Size)
-	return nil
+	var err error
+	*attr, err = GetAttr(d.path)
+	return err
 }
 
 func (d *dir) Lookup(ctx context.Context, name string) (fs.Node, error) {
@@ -128,10 +145,10 @@ func (d *dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 /*
 func (d *dir) Open(ctx context.Context, req *fuse.OpenRequest, resp *fuse.OpenResponse) (fs.Handle, error) {
 	dir_info, _ := os.Lstat(d.path)
-	//_, err := syscall.Open(d.path, int(req.Flags), uint32(dir_info.Mode()))
+	//_,  := syscall.Open(d.path, int(req.Flags), uint32(dir_info.Mode()))
 	open_dir, err := os.OpenFile(d.path, int(req.Flags), dir_info.Mode())
 	//open_dir, err := os.Open(d.path)
-	return &dirhandle{open_dir, d.path}, err
+	return &{open_dir, d.path}, nil
 }
 */
 
@@ -149,13 +166,32 @@ func (d *dir) Create(ctx context.Context, req *fuse.CreateRequest, resp *fuse.Cr
 */
 
 func (f *file) Attr(ctx context.Context, attr *fuse.Attr) error {
-	file_info, _ := os.Lstat(f.path)
-	file_attr := syscall.Stat_t{}
-	syscall.Lstat(f.path, &file_attr)
-	attr.Inode = uint64(file_attr.Ino)
-	attr.Mode = os.FileMode(file_info.Mode())
-	attr.Size = uint64(file_attr.Size)
-	return nil
+	//fmt.Println("//in the attr function//")
+	var err error
+	*attr, err = GetAttr(f.path)
+	return err
+}
+
+func (f *file) Setattr(ctx context.Context, req *fuse.SetattrRequest, resp *fuse.SetattrResponse) error {
+	//fmt.Println("//in the setattr function with file receiver//")
+	sets_attr := []struct {
+		flag   fuse.SetattrValid
+		chattr func()
+	}{
+		{fuse.SetattrSize, func() { os.Truncate(f.path, int64(req.Size)) }},
+		{fuse.SetattrAtime | fuse.SetattrMtime, func() { os.Chtimes(f.path, req.Atime, req.Mtime) }},
+		{fuse.SetattrGid | fuse.SetattrUid, func() { os.Chown(f.path, int(req.Uid), int(req.Gid)) }},
+		{fuse.SetattrMode, func() { os.Chmod(f.path, req.Mode) }},
+	}
+
+	for _, el := range sets_attr {
+		if req.Valid&el.flag != 0 {
+			el.chattr()
+		}
+	}
+	var err error
+	resp.Attr, err = GetAttr(f.path)
+	return err
 }
 
 /*
@@ -171,33 +207,35 @@ func (f *file) ReadAll(ctx context.Context) ([]byte, error) {
 */
 
 func (fh *filehandle) Read(ctx context.Context, req *fuse.ReadRequest, resp *fuse.ReadResponse) error {
-	//open_file, _ := os.OpenFile(f.path, os.O_RDONLY, 0666)
+	//fmt.Println("//in the read function//")
 	buff := make([]byte, req.Size)
 	_, err := fh.open_file.Read(buff)
-	//ReadAt already serves the purpose of seek. so no need for another seek call
-	//fh.open_file.Seek(int64(n), 1)
 	resp.Data = buff
 	return err
 }
 
 func (fh *filehandle) Write(ctx context.Context, req *fuse.WriteRequest, resp *fuse.WriteResponse) error {
+	//fmt.Println("//in the write function//")
 	n, err := fh.open_file.Write(req.Data)
-	fh.open_file.Truncate(int64(n))
+	//fh.open_file.Truncate(int64(n))
 	resp.Size = int(n)
 	return err
 }
 
 func (fh *filehandle) Fsync(ctx context.Context, req *fuse.FsyncRequest) error {
+	//fmt.Println("//in the fsync function//")
 	fh.open_file.Sync()
 	return nil
 }
 
 func (fh *filehandle) Release(ctx context.Context, req *fuse.ReleaseRequest) error {
+	//fmt.Println("//in the release function//")
 	err := fh.open_file.Close()
 	return err
 }
 
 func (f *file) Open(ctx context.Context, req *fuse.OpenRequest, resp *fuse.OpenResponse) (fs.Handle, error) {
+	//fmt.Println("//in the open function//")
 	file_info, err := os.Lstat(f.path)
 	/*if file_info.Mode()&os.ModeSymlink != 0 {
 		target, _ := os.Readlink(f.path)
